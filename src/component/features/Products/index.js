5b90e0806c63853f8ee1912c08ab637268cde4f4
anwarbakkar <anwarbkkr007@gmail.com>
2024-09-20 17:23:06 -0700

    Add card in product page

--- src/component/features/Products/index.js
+++ src/component/features/Products/index.js
@@ -1,9 +1,34 @@
+import { Container, Stack, Typography } from '@mui/material'
 import React from 'react'
+import Card from "../../common/Card"
+import data from '../../../Data'
+import { Link } from 'react-router-dom'
 
-const index = () => {
+const Index = () => {
   return (
-    <div>Products</div>
+    <Container sx={{padding:"5rem 1rem"}}>
+      <Stack direction="row">
+        <Stack flex="1">
+          
+        </Stack>
+        <Stack flex="3">
+          <Stack></Stack>
+          <Stack direction="row" gap="1rem" justifyContent="space-evenly" flexWrap="wrap">
+          {
+           data && data.length? data.map((item) => (
+              <Link to={`/productsDetail/${item.id}`}>
+                <Card data={item}/>
+              </Link>
+            )):
+            <Typography>
+              Curently Data Not Available
+            </Typography>
+          }
+          </Stack>
+        </Stack>
+      </Stack>
+    </Container>
   )
 }
 
-export default index
+export default Index
